84746c5f964ccb08701e3d4336faa2bc891e5e6a
Kenjiro56 <hiroken506@gmail.com>
2024-11-10 18:03:37 +0900

    feat/bucketListView

--- frontend/src/app/bucketList/page.tsx
+++ frontend/src/app/bucketList/page.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 
-const Home: React.FC = () => {
+
+const buckerListView: React.FC = () => {
+  const [bucketItems, setBucketItems] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const userId = 1; //あとで変更できるようにする
+  useEffect(() => {
+    const fetchBucketItems = async () =>{
+      try{
+        const response = await fetch(`http://localhost:8080/bucketls/getAll/${userId}`);
+        const data = await response.json();
+        setBucketItems(data);
+      } catch (error) {
+        console.error('バケットリストの取得に失敗しました:', error);
+      }finally{
+        setLoading(false);
+      }
+    };
+    fetchBucketItems();
+  }, []);
     return (
-        <>
-            bucketListだよーん
-        </>
+        <div>
+          {loading ? (
+            <p>読み込み中...</p>
+          ):(
+            <ul>
+              {bucketItems.map((item) => (
+                <li key={item.id}>{item.bucket_title}</li>
+              ))}
+            </ul>
+          )}
+        </div>
     );
 };
 
-export default Home;
+export default buckerListView;
