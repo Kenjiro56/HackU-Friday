c1d9899de80a6e59a920b137fac46381068ccbd3
Kenjiro56 <hiroken506@gmail.com>
2024-11-11 22:18:42 +0900

    fix/fetchAPI

--- frontend/src/app/bucketList/components/NewBucketModal.tsx
+++ frontend/src/app/bucketList/components/NewBucketModal.tsx
@@ -2,25 +2,47 @@ import React from 'react';
 
 interface ModalProps {
   onClose: () => void;
-  onSubmit: (
-    userId: number,
-    bucketTitle: string,
-    timeId: number,
-    loopFlag: boolean,
-    description: string
-  ) => void;
+
 }
 
-const NewBucketModal: React.FC<ModalProps> = ({ onClose, onSubmit }) => {
+const NewBucketModal: React.FC<ModalProps> = ({ onClose }) => {
   const [bucketTitle, setBucketTitle] = React.useState('');
   const [timeId, setTimeId] = React.useState(0);
   const [loopFlag, setLoopFlag] = React.useState(false);
   const [description, setDescription] = React.useState('');
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
 
   const userId = 1; //あとで変更できるようにする
-  const handleSubmit = () => {
-    onSubmit(userId, bucketTitle, timeId, loopFlag, description);
-    onClose();
+  const handleSubmit = async () => {
+    setIsSubmitting(true);
+    const data = {
+      userId,
+      bucketTitle,
+      timeId,
+      loopFlag,
+      description: description || '',
+    };
+
+    try {
+      const respionse = await fetch('http://localhost:8080/bucketls/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (respionse.ok) {
+        console.log('バケットリストの追加に成功しました');
+        onClose();
+      }else{
+        console.error('バケットリストの追加に失敗しました');
+      }
+    } catch(error){
+        console.error('Error fetching API:', error);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -61,7 +83,7 @@ const NewBucketModal: React.FC<ModalProps> = ({ onClose, onSubmit }) => {
         <textarea
           placeholder="詳細 (任意)"
           value={details}
-          onChange={handleDetailsChange}
+          onChange={handleDescriptionChange}
           maxLength={255}
         ></textarea>
         <p>{255 - details.length} 文字残り</p>
