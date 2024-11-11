d26a99220335a5435146366d3ca1616f2524a19a
Kenjiro56 <hiroken506@gmail.com>
2024-11-12 00:58:37 +0900

    feat/Header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,15 @@
 'use client';
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
+import Logo from '../assets/logo.svg';
 
 
 const Header: React.FC = () => {
   const router = useRouter();
+  const currentPath = usePathname();
 
   const navigateToHome = () => {
-    router.push('/');
+    router.push('/gachaHome');
   };
 
   const navigateToBucketList = () => {
@@ -15,9 +17,16 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header style={{ display: 'flex', justifyContent: 'space-between', padding: '1rem', background: '#000' }}>
-      <button onClick={navigateToHome}>ガチャをひく</button>
-      <button onClick={navigateToBucketList}>やりたいこと</button>
+    <header className="w-full max-w-xs mx-auto flex flex-col items-center p-4 fixed top-0 left-0 right-0 z-10">
+      <Logo width={100} height={50} />
+      <div className="flex w-full rounded-full overflow-hidden border-2 border-black">
+        <button onClick={navigateToHome} className={`flex-1 py-2 ${
+          currentPath === '/gachaHome' ? 'bg-black text-white' : 'bg-white text-black'
+        }`}>ガチャをひく</button>
+        <button onClick={navigateToBucketList} className={`flex-1 py-2 ${
+          currentPath === '/bucketList' ? 'bg-black text-white' : 'bg-white text-black'
+        }`}>やりたいこと</button>
+      </div>
     </header>
   );
 };
