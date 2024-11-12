5ca2fd819a960ab2705f012c9903f980c54c427e
Kenjiro56 <hiroken506@gmail.com>
2024-11-12 19:26:58 +0900

    謎の空白が消せません

--- frontend/src/app/gachaHome/components/DescriptionModal.tsx
+++ frontend/src/app/gachaHome/components/DescriptionModal.tsx
@@ -4,7 +4,10 @@ interface ModalProps {
   onClose: () => void;
 }
 
+
+
 const DescriptionModal: React.FC<ModalProps> = ({ onClose }) => {
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50 z-50">
       <div className="bg-white rounded-xl p-6 w-80 shadow-lg">
