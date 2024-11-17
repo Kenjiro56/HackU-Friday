f30fd6c0b532a6ec58a398084d5912028949c442
Kenjiro56 <hiroken506@gmail.com>
2024-11-17 15:36:22 +0900

    feat/GachaThumbnail_component

--- frontend/src/app/gachaHome/components/GachaThumbnail.tsx
+++ frontend/src/app/gachaHome/components/GachaThumbnail.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
 
-const GachaThumbnail: React.FC = () => {
+interface Props {
+  label: string;
+  srcPath: string;
+  imgWidth: number;
+  imgHeight: number;
+}
+
+const GachaThumbnail: React.FC<Props> = ( props ) => {
   return (
-    <div>
-      <h1>GachaThumbnail</h1>
+    <div className="flex bg-white rounded-[30px] border-2 border-black relative w-[312px] h-[418px] justify-center items-center min-w-full mr-4">
+      <div className="absolute top-3 left-3 bg-white text-[#FCC605] text-sm px-3 py-2 rounded-[100px] border-2 border-[#FCC605] w-[84px] h-[42px] flex justify-center items-center">
+      { props.label }
+      </div>
+      <Image src={ props.srcPath } alt={ props.label } width={ props.imgWidth } height={ props.imgHeight } />
     </div>
   );
 }
